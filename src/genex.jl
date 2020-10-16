ac48cb9e96bd76806f4de4c0cb50b67fb34b91cd
Stefan Wojcik <swojcik@pewresearch.org>
2020-10-15 21:02:12 -0500

    adding main functions

--- src/genex.jl
+++ src/genex.jl
@@ -6,32 +6,51 @@ import Images
 import AWSCore
 import AWSS3
 import OffsetArrays
+import ImageMagick
+# MUST: add CUDA@1.3.3
+import CUDA
 
 aws = AWSCore.aws_config()
 
-# get the keys for all objects #img_paths = get_objects()
-get_objects() = [x["Key"] for x in s3_list_objects(aws, "brazil.images")]
-
-# load a random Image
-get_random_image() = load(IOBuffer(s3_get(aws, "brazil.images", img_paths[rand(1:length(img_paths))])))
-
-# load a specific numbered image
-get_num_image(filenum) = load(IOBuffer(s3_get(aws, "brazil.images", img_paths[filenum])))
-
-# iterate over every image 
-# must be fault tolerant
-# must not leave gaps 
-function proces_all()
-    processed_img_paths = []
-    array_alloc = Array{Float64, 4}[]
-    for x in s3_list_objects(aws, "brazil.images")
-        push!(processed_img_paths, x["Key"])
-        img_raw 
+# create mega_function
+"""
+Loading an image on Linux requires a few dependencies, including ImageMagic. In order to make this work 
+on AWS, I had to run: 
+`wget https://github.com/ImageMagick/ImageMagick/archive/7.0.10-11.tar.gz` (to download a slightly old version of ImageMagick - newer version failed with error)
+then, 
+cd ImageMagick..., then 
+`./configure`, then 
+`make` , then 
+`sudo make install`, then 
+`sudo ldconfig /usr/local/lib`
+Finally, it then appears that you have to use a workaround on Linux machines
+using ImageMagick
+img = readblob(take!(buffer)) # in order to read from a real buffer 
+"""
+function process_data(aws, max_iters)
+    res = ResNet() # load the resnet model 
+    out = Array{Float64,2}[] # array to fill 
+    img_3d = zeros(Float64, (224, 224, 3, 1)) # skeleton array
+    prediction_array = zeros(Float64, (2048, 1))
+    count = 0 # counter for stopping 
+    for img in s3_list_objects(aws, "brazil.images")
+        img_processed = img["Key"] |>
+            x -> s3_get(aws, "brazil.images", x) |>
+            x -> IOBuffer(x) |>
+            x -> readblob(take!(x)) |>
+            x -> Float64.(x) |>
+            x -> padarray(x, Fill(1,(29,52),(30,52))) |>
+            x -> OffsetArray(x, 1:224, 1:224) |>
+            x -> img_3d[:, :, 1:3, :] .= x
+        count += 1
+        pred .= res.layers[1:20](img_processed)
+        push!(out, copy(pred))
+        if count == max_iters
+            break
+        end
     end
-end    
+    return out
+end
 
-greet() = print("Hello World!")
-
-fthis() = print("ffed")
 
 end # module
