74f66a9afff5076c35e3b79540e84fca3372fa4a
= <=>
2021-01-24 18:50:10 -0500

    fix face_locs bug

--- src/genex.jl
+++ src/genex.jl
@@ -82,8 +82,8 @@ function generate_expression_features(face_locations, resnet_model, aws, face_se
         #printstyled(img_key*" \n", color=:green)
         try
             raw_img = download_raw_img(img_key, aws)
-            if !isempty(face_locs[img_key])
-                top, right, bottom, left = face_locs[img_key][1]
+            if !isempty(face_locations[img_key])
+                top, right, bottom, left = face_locations[img_key][1]
                 face_seg_img = raw_img[top:bottom, left:right]
                 body_padded, face_padded = pad_it(raw_img), pad_it(face_seg_img)
                 @inbounds face_features_out[:, i] .= (resnet_model.layers[1:20](face_padded) |> Flux.gpu)[:, 1]
