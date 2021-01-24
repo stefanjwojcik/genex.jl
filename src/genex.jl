b279e6948486c513a3cfd12dc2946bff7ed87175
= <=>
2021-01-24 08:13:28 -0500

    adding pad func and utils for face seg loop

--- src/genex.jl
+++ src/genex.jl
@@ -43,6 +43,30 @@ function download_raw_img(img_key::String, aws)
     return img_processed
 end
 
+"""
+Calculate proper padding size = in this case it's 224 
+"""
+function calculate_img_pad(raw_img_size; size_squared=224)
+    diff = size_squared-raw_img_size
+    split = Int(round((diff)/2)) #calculate padding size for x and y
+    out1, out2 = split, diff-(split*2) + split # dealing with odd cases 
+end
+
+"""
+Pad the image 
+"""
+function pad_it(raw_img)
+    (x1, x2), (y1, y2) = calculate_img_pad.(size(raw_img))
+    img_3d = zeros(Float32, (224, 224, 3, 1))
+    out = raw_img |>
+    x -> Float32.(x) |>
+    x -> Images.padarray(x, Images.Fill(1,(x1,y1),(x2,y2))) |>
+    x -> OffsetArrays.OffsetArray(x, 1:224, 1:224) |>
+    x -> img_3d[:, :, 1:3, :] .= x
+    return out
+end
+
+
 # This is a slightly older way to process the data
 """
 process_aws_link(img_key::String)
