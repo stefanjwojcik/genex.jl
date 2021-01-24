df335b23f238a141159d9d5e0dc49e8da4630fc4
= <=>
2021-01-24 11:13:34 -0500

    adding key functions to genex

--- src/genex.jl
+++ src/genex.jl
@@ -67,6 +67,78 @@ function pad_it(raw_img)
 end
 
 
+# Newer mega-function - uses @inbounds and cuarrays to process the data 
+function generate_expression_features(face_locations, resnet_model; aws=aws, face_seg_file = "face_features.txt", body_file = "body_features.txt")
+    # Progress Meter just to see where we are in the process 
+    p = ProgressMeter.Progress(length(face_locations)) # total of iterations, by 1
+    ProgressMeter.next!(p)
+    failed_cases = String[]
+
+    # Creating empty arrays to store the results 
+    face_features_out = zeros(Float32, 2048, length(face_locations)) # create base 
+    body_features_out = zeros(Float32, 2048, length(face_locations))
+
+    for (i, img_key) in enumerate(keys(face_locations))
+        #printstyled(img_key*" \n", color=:green)
+        try
+            raw_img = download_raw_img(img_key, aws)
+            if !isempty(face_locs[img_key])
+                top, right, bottom, left = face_locs[img_key][1]
+                face_seg_img = raw_img[top:bottom, left:right]
+                body_padded, face_padded = pad_it(raw_img), pad_it(face_seg_img)
+                @inbounds face_features_out[:, i] .= (resnet_model.layers[1:20](face_padded) |> Flux.gpu)[:, 1]
+                @inbounds body_features_out[:, i] .= (resnet_model.layers[1:20](body_padded) |> Flux.gpu)[:, 1]
+            else 
+                body_padded = pad_it(raw_img)
+                @inbounds body_features_out[:, i] .= (resnet_model.layers[1:20](body_padded) |> Flux.gpu)[:, 1]
+                @inbounds face_features_out[:, i] .= body_features_out[i, :] # substitute face w/ body if empty 
+            end
+        catch
+            @warn "$img_key failed"
+            push!(failed_cases, img_key)
+            @inbounds face_features_out[:, i] .= zeros(2048, )
+            @inbounds body_features_out[:, i] .= zeros(2048, )
+        end
+
+        ProgressMeter.next!(p)
+    end
+    # Write out the files
+    out = (body_features_out, face_features_out, failed_cases)
+    printstyled("DONE \n", color=:blue)
+    return out
+end
+
+function write_out_result(face, body; face_path= "face.txt", body_path ="body.txt")
+    writable_face = open(face_path, "w+")
+    writable_body = open(body_path, "w+")
+
+    # Ping the writable files to initialize them with appropriate sizes
+    write(writable_face, size(face, 1)); write(writable_face, size(face, 2))
+    write(writable_body, size(body, 1)); write(writable_body, size(body, 2))
+
+    # Write out 
+    for col in 1:size(face)[2]
+        write(writable_face, face[col, :])
+    end
+
+    for col in 1:size(body)[2]
+        write(writable_body, body[col, :])
+    end
+    # Close the files 
+    close(writable_face)
+    close(writable_body)
+
+end
+
+function read_in_result(path)
+    s = open(path) # default is read-only
+    m = read(s, Int)
+    n = read(s, Int)
+    out = Mmap.mmap(s, Matrix{Int}, (m,n))
+    return out
+end
+
+
 # This is a slightly older way to process the data
 """
 process_aws_link(img_key::String)
