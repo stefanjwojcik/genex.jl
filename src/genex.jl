5c17a1208dc5635f4106f0e1feadd3d4464ad395
= <=>
2021-07-30 23:37:23 -0400

    updated processing to match python

--- src/genex.jl
+++ src/genex.jl
@@ -25,6 +25,7 @@ import ImageMagick
 # MUST: add CUDA@1.3.3
 import CUDA
 import ProgressMeter
+import PyCall
 
 #aws = AWSCore.aws_config()
 
@@ -43,44 +44,28 @@ function download_raw_img(img_key::String, aws)
     return img_processed
 end
 
-"""
-preprocess_input(x::Array{ColorTypes.Gray{FixedPointNumbers.Normed{UInt8,8}},2})
-Takes raw downloaded image, and applies same preprocessing as python's Keras (normalized by)
-mean of ImageNet)
-"""
-function preprocess_input(input::Array{Float32,4})
-    imagenet_means = [103.939, 116.779, 123.68]
-    x = input .* 255 
-        x[:, :, 1, 1] .= x[:, :, 1, 1] .- imagenet_means[1] 
-        x[:, :, 2, 1] .= x[:, :, 2, 1] .- imagenet_means[2]
-        x[:, :, 3, 1] .= x[:, :, 3, 1] .- imagenet_means[3] 
-    return x
-end
+py"""
+from tensorflow.keras.applications.vgg19 import preprocess_input
 
+def tf_preprocess(x):
+    return preprocess_input(x)
+"""
 
-"""
-Calculate proper padding size = in this case it's 224 
-"""
-function calculate_img_pad(raw_img_size; size_squared=224)
-    diff = size_squared-raw_img_size
-    split = Int(round((diff)/2)) #calculate padding size for x and y
-    out1, out2 = split, diff-(split*2) + split # dealing with odd cases 
-end
+py"""
+import numpy as np
 
+def my_expand(x):
+    return np.expand_dims(x, axis=0)
 """
-Pad the image, and add pre-processing step as seen above 
-"""
-function pad_it(raw_img)
-    (x1, x2), (y1, y2) = calculate_img_pad.(size(raw_img))
-    img_3d = zeros(Float32, (224, 224, 3, 1))
+
+function my_process(raw_img)
     out = raw_img |>
+    x -> imresize(x, 224, 224) |>
     x -> Float32.(x) |>
-    x -> Images.padarray(x, Images.Fill(1,(x1,y1),(x2,y2))) |>
-    x -> OffsetArrays.OffsetArray(x, 1:224, 1:224) |>
-    x -> @inbounds img_3d[:, :, 1:3, :] .= x
-    return preprocess_input(img_3d)
+    x -> py"np.expand_dims(x, axis=0)" |>
+    x -> py"tf_preprocess"(x) 
+    return out[1, :, :, :, :]
 end
-
 
 # Newer mega-function - uses @inbounds and cuarrays to process the data 
 function generate_expression_features(face_locations, resnet_model, aws)
@@ -100,11 +85,11 @@ function generate_expression_features(face_locations, resnet_model, aws)
             if !isempty(face_locations[img_key])
                 top, right, bottom, left = face_locations[img_key][1]
                 face_seg_img = raw_img[top:bottom, left:right] #bug if locations include 0
-                body_padded, face_padded = pad_it(raw_img), pad_it(face_seg_img)
+                body_padded, face_padded = my_process(raw_img), my_process(face_seg_img)
                 @inbounds face_features_out[:, i] .= (resnet_model.layers[1:20](face_padded) |> Flux.gpu)[:, 1]
                 @inbounds body_features_out[:, i] .= (resnet_model.layers[1:20](body_padded) |> Flux.gpu)[:, 1]
             else 
-                body_padded = pad_it(raw_img)
+                body_padded = my_process(raw_img)
                 @inbounds body_features_out[:, i] .= (resnet_model.layers[1:20](body_padded) |> Flux.gpu)[:, 1]
                 @inbounds face_features_out[:, i] .= body_features_out[:, i] # substitute face w/ body if empty 
             end
@@ -123,51 +108,10 @@ function generate_expression_features(face_locations, resnet_model, aws)
     return out
 end
 
-# This is a slightly older way to process the data
-"""
-process_aws_link(img_key::String)
-takes an image link from AWS bucket, drops it into a buffer oject, converts to float32, then pads it for a perfect fit into the right
-dimensions. Finally, it converts it into a 3d object for rbg types. 
-"""
-function process_aws_link(img_key::String, aws)
-    img_3d = zeros(Float32, (224, 224, 3, 1)) # skeleton array
-    img_processed = img_key |>
-        x -> AWSS3.s3_get(aws, "brazil.images", x) |>
-        x -> IOBuffer(x) |>
-        x -> ImageMagick.readblob(take!(x)) |>
-        x -> Float32.(x) |>
-        x -> Images.padarray(x, Images.Fill(1,(29,52),(30,52))) |>
-        x -> OffsetArrays.OffsetArray(x, 1:224, 1:224) |>
-        x -> img_3d[:, :, 1:3, :] .= x
-    return img_processed
-end
 
- # This function looks at the raw image alone - no face recogntion 
-function compress_images(bucket, model, aws; test=true)
-    out = Array{Float32,2}[] # array to fill 
-    prediction_array = zeros(Float32, (2048, 1)) #skeleton array to keep replacing
-    nit = 0 # counter for stopping 
-    max_iters = test ? 25 : 193_108 #ternary operator 
-    p = ProgressMeter.Progress(max_iters) # total of iterations, by 1
-    ProgressMeter.next!(p)
-    img_links = [x["Key"] for x in bucket]
-    for ik in img_links
-        nit += 1
-        ProgressMeter.next!(p)
-        if (test & (nit >= max_iters))
-            break
-        end 
-        preproc_img = process_aws_link(ik, aws)
-        prediction_array .= model.layers[1:20](preproc_img) |> Flux.gpu
-        push!(out, copy(prediction_array))
-    end
-    return out
-end
 
-export process_aws_link,
-       compress_images, 
-       download_raw_img, 
-       pad_it, 
+export download_raw_img, 
+       my_process, 
        generate_expression_features
 
 end # module
